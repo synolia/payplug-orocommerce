539b95a62331a90673d478a3d01206f4dafc585d
Kevin Hilderal <khl@synolia.com>
2020-07-06 10:44:14 +0200

    Use alias to set an oro service public

--- src/PayplugPaymentBundle.php
+++ src/PayplugPaymentBundle.php
@@ -2,18 +2,8 @@
 
 namespace Payplug\Bundle\PaymentBundle;
 
-use Payplug\Bundle\PaymentBundle\DependencyInjection\OverrideCompilerPass;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class PayplugPaymentBundle extends Bundle
 {
-    /**
-     * @param ContainerBuilder $container
-     */
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-        $container->addCompilerPass(new OverrideCompilerPass());
-    }
 }
