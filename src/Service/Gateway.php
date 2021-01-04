f12d84f2d4d2d40d413bd93c8081e3d92ee23f72
syno_jvi <julien.vinai@synolia.com>
2021-01-04 17:47:11 +0100

    Add metadata infos on payment create and refund

--- src/Service/Gateway.php
+++ src/Service/Gateway.php
@@ -140,6 +140,7 @@ class Gateway
     public function createPayment(PaymentTransaction $paymentTransaction, PayplugConfigInterface $config)
     {
         $payplugClient = $this->initPayplugClientAndSetDebugModeForLogger($config);
+        $routerContext = $this->router->getContext();
 
         $this->logger->debug(__METHOD__ . ' BEGIN');
 
@@ -149,10 +150,15 @@ class Gateway
             'shipping' => $this->getAddressValues(AddressType::TYPE_SHIPPING, $paymentTransaction),
             'billing' => $this->getAddressValues(AddressType::TYPE_BILLING, $paymentTransaction),
             'hosted_payment' => $this->getCallbackUrls($paymentTransaction),
-            'notification_url' => $this->getNotificationUrl($paymentTransaction)
+            'notification_url' => $this->getNotificationUrl($paymentTransaction),
+            'metadata' => [
+                'order_id' => $paymentTransaction->getEntityIdentifier(),
+                'customer_id' => $paymentTransaction->getFrontendOwner()->getId(),
+                'website' => sprintf('%s://%s', $routerContext->getScheme(), $routerContext->getHost()),
+            ]
         ];
 
-        $this->logger->debug('Payment::create from data ' . $this->logger->anonymizeAndJsonEncodeArray($data));
+        $this->logger->error('Payment::create from data ' . $this->logger->anonymizeAndJsonEncodeArray($data));
 
         try {
             $payment = Payment::create($data, $payplugClient);
@@ -179,8 +185,18 @@ class Gateway
 
         $this->logger->debug(__METHOD__ . ' BEGIN');
         $amountToRefund = (int) round($amount * 100);
+        $routerContext = $this->router->getContext();
 
         $refund = null;
+
+        $data = [
+            'amount'   => $amountToRefund,
+            'metadata' => [
+                'order_id' => $paymentTransaction->getEntityIdentifier(),
+                'customer_id' => $paymentTransaction->getFrontendOwner()->getId(),
+                'website' => sprintf('%s://%s', $routerContext->getScheme(), $routerContext->getHost()),
+            ]
+        ];
 
         try {
             $refund = Refund::create($paymentTransaction->getReference(), ['amount' => $amountToRefund], $payplugClient);
